914289c5357cd1115c601d6a1e474cad384b5c09
José Gomes <148223610+gomessaneto@users.noreply.github.com>
2024-02-27 09:31:42 -0300

    Update Calendario.java
    tirei o set da variavel array da classe main

--- questaoNove/Calendario.java
+++ questaoNove/Calendario.java
@@ -5,7 +5,6 @@ public class Calendario {
 	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner (System.in);
-		String[] meses = {"janeiro", "fevereiro", "marco", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"};	
 		
 		System.out.println("Insira o numero do mes(contando a partir do 0. exemplo: janeiro mes 0 - fevereiro mes 1): ");
 		int mes = scanner.nextInt();
